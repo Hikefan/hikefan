c8089833dc43b9b6d630882dc97309f021345bae
Danish Jain <danishjain08@gmail.com>
2018-02-08 21:28:36 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function (event) {
-	if (location.pathname.replace(/^/ / , '') == this.pathname.replace(/^/ / , '') && location.hostname == this.hostname) {
+	
 		var target = $(this.hash);
 		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 		if (target.length) {
@@ -17,5 +17,5 @@ $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function (event) {
 				};
 			});
 		}
-	}
+	
 });
